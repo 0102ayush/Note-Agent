0e7c12238ccaf5f632982ffc81b6468cc981dbf1
Ayush Yadav <129136683+0102ayush@users.noreply.github.com>
2025-06-23 17:32:21 +0530

    Revert "Added backend!"

--- backend/config/config.py
+++
@@ -1,5 +0,0 @@
-# C:\Users\ashut\OneDrive\Desktop\Note-Agent-main\backend\config\config.py
-class Config:
-    DATABASE_URL = "C:/Users/ashut/OneDrive/Desktop/Note-Agent-main/backend/notes.db"  # Absolute path with forward slashes
-    AUDIO_DIR = "./audio_files"
-    EXPORT_DIR = "./exports"
